5a468c51198022f090c3aa00b35df05f32705bec
Alexey Romanov <romanov.alexey2000@gmail.com>
2019-09-01 15:00:12 +0300

    Delete test.py

--- BotFunc/test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from functools import reduce
-arr_msg = [x for x in range(11)]
-arr_msg = list(map(str, arr_msg))
-
-fin_msg = "XXX: "
-print(list(range(1, len(arr_msg), 2)))
-print(reduce(lambda x, y: x + arr_msg[y] + " " + arr_msg[y + 1] + " ", range(1, len(arr_msg), 2), fin_msg))
